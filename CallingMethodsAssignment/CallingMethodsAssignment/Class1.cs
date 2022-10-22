e02e5e86fd79e78b64ac6ecee23686b8aa8c16fb
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-10-21 18:29:32 -0700

    Update calling methods assignment

--- CallingMethodsAssignment/CallingMethodsAssignment/Class1.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Class1.cs
@@ -8,21 +8,22 @@ namespace CallingMethodsAssignment
 {
    public class Class1
     {
-      public double Add(int data)//method named 'Add' is public. Double is able to define variables holding numbers with deicmal points. Passes in the integer data
+      public int Add(int data)//method named 'Add' is public, takes in int parameter and passes in the integer data
         {
             data = data + 7;//data is equal to user input + 7
-            return Divide(data);//return to divide method 
+            return data;//returns 'data'
         }
 
-        public double Divide(double data)//method name 'Divide' is public, while also passing in 'data'
+        public int Divide(int data2)//method name 'Divide' is public, while also passing in 'data2'
         {
-            data = data - 2;//data is equal to the passed in data minus 2
-            return Multiply(data);//return to multiply method
+            data2 = data2 - 2;//data2 is equal to the userinput minus 2
+            return data2;//returns 'data2'
         }
         
-        public double Multiply(double data)//method name 'Multiply' is public, while passing in 'data'
+        public int Multiply(int data3)//method name 'Multiply' is public, while passing in 'data3'
         {
-            return data * 4;//returns 'data' times 4
+            data3 = data3 * 4;//returns 'data3', which is the users input and multiplies it by 4
+            return data3;
         }
        
     }
--- CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -9,14 +9,22 @@ namespace CallingMethodsAssignment
     class Program
     {
         public static int Data { get; set; } //public, returns value of variable name
+        public static int Data2 { get; set; }
+        public static int Data3 { get; set; }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter input:");
             string line = Console.ReadLine();//user input saved into variable 'line'
             Data = Int32.Parse(line);//converts 'line' to int and saves under 'Data'
+            Data2 = Int32.Parse(line);//converts 'line' to int and saves under 'Data2'
+            Data3 = Int32.Parse(line);//converts 'line' to int and saves under 'Data3'
             var operatorObject = new Class1();//variable is equal to new Class1();
             var result = operatorObject.Add(Data);//result variable is equal to Class1, saved under operatorObject, and adding variable data
-            Console.WriteLine(result);//display variable result
+            var result2 = operatorObject.Divide(Data2);//result2 variable is equal to Class1, saved under operatorObject, and adding variable data2
+            var result3 = operatorObject.Multiply(Data3);//result3 variable is equal to Class1, saved under operatorObject, and adding variable data3
+            Console.WriteLine(result);//display varaible result
+            Console.WriteLine(result2);//display variable result2
+            Console.WriteLine(result3);//display variable result3
             Console.ReadLine();//ensures console remains open
 
           
