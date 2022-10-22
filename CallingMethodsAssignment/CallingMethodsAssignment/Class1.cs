9d07e91fb7cb891c40b480ff160aca89c8b14862
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-10-21 17:05:22 -0700

    Upload calling methods assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+   public class Class1
+    {
+      public double Add(int data)//method named 'Add' is public. Double is able to define variables holding numbers with deicmal points. Passes in the integer data
+        {
+            data = data + 7;//data is equal to user input + 7
+            return Divide(data);//return to divide method 
+        }
+
+        public double Divide(double data)//method name 'Divide' is public, while also passing in 'data'
+        {
+            data = data - 2;//data is equal to the passed in data minus 2
+            return Multiply(data);//return to multiply method
+        }
+        
+        public double Multiply(double data)//method name 'Multiply' is public, while passing in 'data'
+        {
+            return data * 4;//returns 'data' times 4
+        }
+       
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        public static int Data { get; set; } //public, returns value of variable name
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+            string line = Console.ReadLine();//user input saved into variable 'line'
+            Data = Int32.Parse(line);//converts 'line' to int and saves under 'Data'
+            var operatorObject = new Class1();//variable is equal to new Class1();
+            var result = operatorObject.Add(Data);//result variable is equal to Class1, saved under operatorObject, and adding variable data
+            Console.WriteLine(result);//display variable result
+            Console.ReadLine();//ensures console remains open
+
+          
+        }
+    }
+}
