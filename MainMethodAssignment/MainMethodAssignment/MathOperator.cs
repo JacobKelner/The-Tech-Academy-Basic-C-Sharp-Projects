7afce67bceb15c9e6c806a33137f512700fda63e
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-10-24 16:01:16 -0700

    Upload main methods assignment

---
+++ MainMethodAssignment/MainMethodAssignment/MathOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+   public class MathOperator
+    {
+        public int Add(int data)//public method integer Add taking in integer data
+        {
+            data = data + 7;//data equals data plus 7 
+            return data;//returns data
+        }
+        public int Add(decimal data2)//public method integer Add taking in decimal data2
+        {
+            int x = Convert.ToInt32(data2);//creates integer variable and saves data to to it, after its converted to integer 
+            int result = x + 2;//creates integer variable named 'result' and has it equal to 'x' + 2
+            return result;//returns variable 'result' 
+        }
+        public int Add(string data3)//public method integer Add taking in string data3
+        {
+            int y = Convert.ToInt32(data3);//creates integer variable 'y' and converts data3(string) to integer
+            int result = y + 3;//creates integer variable 'result' equal to 'y' + 3
+            return result;//returns variable 'result'
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer :");//Displays message to user
+            int num1 = Convert.ToInt32(Console.ReadLine());//creates integer variable and converts user input to integer
+            MathOperator obj1 = new MathOperator();//creates object to equal new class
+            Console.WriteLine(obj1.Add(num1));//Displays obj1 and calls method add taking in num1
+            Console.WriteLine("Enter in a decimal: ");//displays message to user 
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());//creates decimal variable and converts user input to deicmal 
+            Console.WriteLine(obj1.Add(num2));//displays objj and calls method add taking in num2
+            Console.WriteLine("Enter a number: ");//displays message to user
+            string num3 = Console.ReadLine();// creates string variable and saves user input to it 
+            Console.WriteLine(obj1.Add(num3));//displays obj1 and calls method add taking in num3
+            Console.ReadLine();//ensures console remains open
+        }
+    }
+}
