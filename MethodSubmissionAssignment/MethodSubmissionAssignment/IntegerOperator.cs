9881fa601b2a332c86b860f8854e3bfe91ca2693
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-10-25 00:04:35 -0700

    Upload unfinished Methods assignment

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/IntegerOperator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class IntegerOperator
+    {
+        public int Obj(int data, int optionalint = 10)
+        {
+            int result = optionalint + data;
+            return result;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegerOperator obj1 = new IntegerOperator();
+            Console.WriteLine("Please input two numbers, one at a time: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(obj1.Obj(num1));
+           
+            Console.ReadLine();
+        }
+    }
+}
