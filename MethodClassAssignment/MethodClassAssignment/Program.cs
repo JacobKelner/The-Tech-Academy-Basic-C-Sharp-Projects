88cc45cfa43d38cb35d1ad801eae0633fd2214bf
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-10-26 18:24:58 -0700

    Upload Method Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod obj1 = new VoidMethod();//creates object to equal new class
+            obj1.Class(2, 7);//calls obj1 in 'Class' while also passing in two numbers
+            obj1.Class(data: 2, data2: 7);//calls obj1 in 'Class' while passing in parameters by their name
+            
+            //Dont need 'Console.ReadLine()' because it is in VoidMethod.cs, which is what is being used to display our parameters, while our parameters are being defined in Program.cs
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class VoidMethod
+    {
+        public void Class(int data, int data2)//public void method taking in two integers as parameters
+        {
+            int result = data + 2;//'result' equals data + 2 (operation on first integer)
+            Console.WriteLine(data2);//displays second integer to screen
+            Console.ReadLine();//ensures console remains open
+        }
+    }
+}
