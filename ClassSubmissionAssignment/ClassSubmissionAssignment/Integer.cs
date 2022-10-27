a77f20cd0f862a00173758b7069b1d62cd78586e
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-10-27 00:26:47 -0700

    Upload class submission assignment

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Integer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Integer
+    {
+        public void Class1(int data)//method with integer parameter
+        {
+            int result = data / 2;//declares int variable 'result' to equal data divided by 2
+            Console.WriteLine(result);//display result to screen
+            Console.ReadLine();//ensures console remains open
+        }
+        public void Class1(int data, int howManyTimes = 2)//overloaded method with different parameters
+        {
+            for (int i = 0; i < howManyTimes; i++)//for loop 
+            {
+                while (howManyTimes > 1)//while howManyTimes is greater than 1, execute below code
+                {
+                    int result = data / 2;//declares int variable 'result' to equal data divided by 2
+                    Console.WriteLine(result);//displays variable 'result'
+                    Console.ReadLine(); //ensures console remains open
+
+                }
+            }
+        }
+    }
+}
---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    static class Program//class declared static
+    {
+        static void Main(string[] args)
+        {
+            Integer obj1 = new Integer();//instantiated class
+            Console.WriteLine("Please input a number: ");//displays message to user
+            int user = Convert.ToInt32(Console.ReadLine());//converts user input to integer and saves to integer variable 'user'
+            obj1.Class1(user);//calls class along with method taking in variable 'user'
+        }
+    }
+}
