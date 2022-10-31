82b575a4a311a5ba965ca11937106959e307b1fb
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-10-30 23:57:21 -0700

    Upload Class Submission Assignment

--- ClassSubmissionAssignment/ClassSubmissionAssignment/Integer.cs
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Integer.cs
@@ -12,20 +12,21 @@ namespace ClassSubmissionAssignment
         {
             int result = data / 2;//declares int variable 'result' to equal data divided by 2
             Console.WriteLine(result);//display result to screen
-            Console.ReadLine();//ensures console remains open
+            //Console.ReadLine();//ensures console remains open
         }
-        public void Class1(int data, int howManyTimes = 2)//overloaded method with different parameters
+        public void Class1(int data, int howManyTimes)//overloaded method with different parameters
         {
             for (int i = 0; i < howManyTimes; i++)//for loop 
             {
-                while (howManyTimes > 1)//while howManyTimes is greater than 1, execute below code
-                {
-                    int result = data / 2;//declares int variable 'result' to equal data divided by 2
-                    Console.WriteLine(result);//displays variable 'result'
-                    Console.ReadLine(); //ensures console remains open
-
-                }
+                int result = data / 2;//declares int variable 'result' to equal data divided by 2
+                Console.WriteLine(result);//displays variable 'result'
+                //Console.ReadLine(); //ensures console remains open
             }
+        }
+        public void Class1(out int data)
+        {
+            data = 60;
+            //Console.ReadLine();
         }
     }
 }
--- ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -14,6 +14,8 @@ namespace ClassSubmissionAssignment
             Console.WriteLine("Please input a number: ");//displays message to user
             int user = Convert.ToInt32(Console.ReadLine());//converts user input to integer and saves to integer variable 'user'
             obj1.Class1(user);//calls class along with method taking in variable 'user'
+            obj1.Class1(user, 3);//calls class along with method taking in vaiable 'user', explicitly passing in the value 3
+            Console.ReadLine();
         }
     }
 }
