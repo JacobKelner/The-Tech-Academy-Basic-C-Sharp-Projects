c49e925b650def41403780ec4960110159f3c515
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-11-04 16:31:21 -0700

    Upload Method and Objects Assignment

---
+++ MethodsAndObjectAssignment/MethodsAndObjectAssignment/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectAssignment
+{
+    public class Person
+    {
+        public List<string> FullNames { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public void SayName()
+        {
+            foreach (string names in FullNames)
+            {
+                Console.WriteLine(names);
+            }
+        }
+    }
+}
---
+++ MethodsAndObjectAssignment/MethodsAndObjectAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FullNames = new List<string> { "Sample", "Student" };
+            //Employee.SayName();
+            Console.ReadLine();
+            
+        }
+    }
+}
