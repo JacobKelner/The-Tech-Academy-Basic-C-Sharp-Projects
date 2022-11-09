d8c5f494e97c7b7c40fff5e67afd3d2b9c9a89fa
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-11-09 01:40:44 -0800

    Upload Polymorphism Assignment

--- AbstractClassAssignment/AbstractClassAssignment/Employee.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace AbstractClassAssignment
 {
-    class Employee : Person//class Employee is inheriting Person class
+    class Employee : Person, IQuittable//class Employee is inheriting Person class
     {
         public override void SayName()//creates SayName() method, override required for inherited abstract class
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);//displays message, along with properties created in Person, and defined in Program
             Console.ReadLine();//ensures console remains open
         }
-
-
-
+        public void Quit(Person person);
+        
+        
+        
     }
 }
--- AbstractClassAssignment/AbstractClassAssignment/Person.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -11,6 +11,7 @@ namespace AbstractClassAssignment
         public string firstName { get; set; }//public string property called 'firstName'
         public string lastName { get; set; }//public string property called 'lastName'
 
+
         public abstract void SayName();//gives abstract 'SayName()' method
     }
 }
