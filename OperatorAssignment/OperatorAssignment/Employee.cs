54f5bc3c02103c00bd7b5f555595d874e0fa5271
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-11-15 19:18:54 -0800

    Upload operators assignment

---
+++ OperatorAssignment/OperatorAssignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignment
+{
+    public class Employee//public class Employee
+    {
+        public string Id { get; set; }//Id property
+        
+        public string FirstName { get; set; }//FirstName property
+        public string LastName { get; set; }//LastName property
+
+        public static bool operator == (Employee employee1, Employee employee2)//overloaded bool operator '==' taking in employee1 and employee2
+        {
+            Console.WriteLine("This is comparing employee1 and employee2 Id property, and displaying 'true' if they equal each other, and false if the Id property values do not equal each other");
+            Console.WriteLine("");
+            return employee1.Id == employee2.Id;//returns bool value of employee1 id and employee2 id (true or false)
+        }
+        public static bool operator!= (Employee employee1, Employee employee2)//overloaded bool operator '!=' taking in employee1 and employee2
+        {
+            Console.WriteLine("This is comparing employee1 and employee2 Id property, and displaying 'true' if they do not equal each other, and false if the Id property values do equal each other");
+            Console.WriteLine("");
+            return employee1.Id != employee2.Id;//returns bool value of employee1 id and employee2 id (true or false)
+        }
+
+    }
+}
---
+++ OperatorAssignment/OperatorAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();//instantiate Employee object 'employee1'
+            employee1.Id = "1";//sets value of Id property to 1
+            employee1.FirstName = "James";//sets value of FirstName property to 'James'
+            employee1.LastName = "Holiday";//sets value of Last Name property to 'Holiday'
+
+            Employee employee2 = new Employee();//instantiates Employee object 'employee2'
+            employee2.Id = "2";//sets value of employee1.Id property to 2
+            employee2.FirstName = "James";//sets value of FirstName property to 'James'
+            employee2.LastName = "Simmons";//sets value of LastName property to 'Simmons'
+
+            Console.WriteLine(employee1 == employee2);//display to console bool value of 'is employee1 equal to employee2?'
+            Console.WriteLine(employee1 != employee2);//display to console bool value of 'is employee1 not equal to employee2?'
+
+            Console.ReadLine();//ensures console remains open
+
+
+        }
+    }
+}
