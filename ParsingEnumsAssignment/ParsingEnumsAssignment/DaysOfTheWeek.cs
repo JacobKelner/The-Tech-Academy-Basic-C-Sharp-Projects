19362e5e4047dfb033eda554f2693c284baf4c90
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-11-17 18:52:30 -0800

    Upload parsing enums assignment

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/DaysOfTheWeek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    public class DaysOfTheWeek
+    {
+        public enum Day//public enum called 'Day'
+        { 
+            //List of days of the week
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try//try block 
+            {
+                Console.WriteLine("Please enter the current day of the week: ");//displays message asking user to input the day of the week
+                string userInput = Console.ReadLine();//saves user input as string variable 'userInput'
+                DaysOfTheWeek.Day day =(DaysOfTheWeek.Day)Enum.Parse(typeof(DaysOfTheWeek.Day), userInput);//creates variable 'day' from 'DaysOfTheWeek.Day' and parses it by defining its type, and what is being parsed, also passing in userInput
+                Console.WriteLine("Have a great " + day + "!");//Displays message 
+            }
+            catch//catch block, executes if above block runs into an error
+            {
+                Console.WriteLine("Please enter an actual day of the week.");//Displays message
+            }
+            Console.ReadLine();//Ensures console remains open 
+        }
+    }
+}
