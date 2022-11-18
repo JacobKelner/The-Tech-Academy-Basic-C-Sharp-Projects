518d6bcecc4c085a1736b41e80780db1bc30eaa1
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-11-18 00:09:10 -0800

    Upload Struct Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();//creates object of data type 'Number' 
+            Number number2 = number;//assigns data type 'number2' to equal 'number'
+            number2.Amount = 23;//sets 'number2.Amount' (amount being the property defined in 'Number.cs') and assigns it a value of 23
+            Console.WriteLine(number2.Amount);//Displays number2.Amount to console
+            Console.ReadLine();//ensures console remains open 
+        }
+    }
+}
