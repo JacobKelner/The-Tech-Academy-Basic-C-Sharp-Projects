36138dbaaac41700c0094626ef06addaeb3cd127
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-12-12 02:17:03 -0800

    Upload Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Foo
+    {
+        public int year;//property/field called year
+        public string model;//property//field called model
+
+        public Foo()//creates a class constructor for the Foo class
+        {
+            model = "Raptor";//define field name
+            year = 2010;//define field name
+        }
+            
+        
+    
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo Ford = new Foo();//creates object called Ford from Foo class, this will call the constructor
+
+            Console.WriteLine("Ford started developing the " + Ford.model + " in the year " + Ford.year + ".");//Ford.model and Ford.year are being defined from the properties in Class1.cs
+            Console.ReadLine();
+
+
+        }
+    }
+}
